d20532a81d3f73fdbfcbac28bfb884d923b1e17d
Loopiezlol <catalin@enki.com>
2018-10-11 11:32:19 +0100

    :art: entry point prompt to ask which generator to use

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,36 @@
-const generators = require('yeoman-generator')
+const generators = require("yeoman-generator");
 
 module.exports = generators.Base.extend({
-  'initializing': function () {
-    this.composeWith('enki:insight')
+  initializing: function() {
+    return this.prompt([
+      {
+        type: "list",
+        name: "subGenerator",
+        message: "What type of content would you like to create?",
+        choices: [
+          { name: "A topic", value: "topic" },
+          { name: "A course", value: "course" },
+          { name: "A workout", value: "workout" },
+          { name: "An insight, game or exercise", value: "insight" }
+        ],
+        default: 4
+      }
+    ]).then(answers => {
+      switch (answers.subGenerator) {
+        case "topic":
+          this.composeWith("enki:topic");
+          break;
+        case "course":
+          this.composeWith("enki:course");
+          break;
+        case "workout":
+          this.composeWith("enki:workout");
+          break;
+        case "insight":
+        default:
+          this.composeWith("enki:insight");
+      }
+      return Promise.resolve();
+    });
   }
-})
+});
